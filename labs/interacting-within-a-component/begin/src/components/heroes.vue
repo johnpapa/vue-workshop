3035529f5def3903a1f866d33e2e1a2307e0663a
John Papa <john@johnpapa.net>
2019-05-18 21:26:44 -0400

    init the begin state of interacting lab

--- labs/interacting-within-a-component/begin/src/components/heroes.vue
+++ labs/interacting-within-a-component/begin/src/components/heroes.vue
@@ -1,107 +1,53 @@
 <template>
   <div>
     <div class="section content-title-group">
-      <h2 class="title">Heroes</h2>
+      <h2 class="title">Hero</h2>
       <div class="columns is-multiline is-variable">
-        <div class="column is-8" v-if="heroes">
-          <ul v-if="!selectedHero">
-            <li
-              v-for="hero in heroes"
-              :key="hero.id"
-            >
-              <div class="card">
-                <div class="card-content">
-                  <div class="content">
-                    <div :key="hero.name" class="name">{{ hero.name }}</div>
-                    <div>{{ hero.description }}</div>
-                  </div>
+        <div class="column is-8">
+          <div class="card edit-detail" v-if="hero && hero.id">
+            <header class="card-header">
+              <p class="card-header-title">{{ fullName }}</p>
+            </header>
+            <div class="card-content">
+              <div class="content">
+                <div class="field">
+                  <label class="label" for="id">id</label>
+                  <label class="input" name="id" readonly>{{hero.id}}</label>
                 </div>
-                <footer class="card-footer">
-                  <button class="link card-footer-item save-button" @click="selectHero(hero)">
-                    <i class="fas fa-check"></i>
-                    <span>Select</span>
-                  </button>
-                </footer>
-              </div>
-            </li>
-          </ul>
-          <div v-if="selectedHero">
-            <div class="card edit-detail">
-              <header class="card-header">
-                <p class="card-header-title">{{ selectedHero.name }}</p>
-              </header>
-              <div class="card-content">
-                <div class="content">
-                  <div class="field">
-                    <label class="label" for="id">id</label>
-                    <label class="input" name="id" readonly>{{selectedHero.id}}</label>
-                  </div>
-                  {{selectedHero.fullAddress}}
-                  <div class="field">
-                    <label class="label" for="fullAddress">Full Address</label>
-                    <label class="input" name="fullAddress" readonly>{{fullAddress}}</label>
-                  </div>
-                  <div class="field">
-                    <label class="label" for="name">name</label>
-                    <input
-                      class="input"
-                      name="name"
-                      placeholder="e.g. Colleen"
-                      type="text"
-                      v-model="selectedHero.name"
-                    >
-                  </div>
-                  <div class="field">
-                    <label class="label" for="description">description</label>
-                    <input
-                      class="input"
-                      name="description"
-                      placeholder="dance fight!"
-                      type="text"
-                      v-model="selectedHero.description"
-                    >
-                  </div>
-                  <div class="field">
-                    <label class="label" for="address">address</label>
-                    <input
-                      class="input"
-                      name="address"
-                      type="text"
-                      v-model="selectedHero.address"
-                    >
-                  </div>
-                  <div class="field">
-                    <label class="label" for="city">city</label>
-                    <input
-                      class="input"
-                      name="city"
-                      type="text"
-                      v-model="selectedHero.city"
-                    >
-                  </div>
-                  <div class="field">
-                    <label class="label" for="state">state</label>
-                    <input
-                      class="input"
-                      name="state"
-                      type="text"
-                      v-model="selectedHero.state"
-                    >
-                  </div>
+                <div class="field">
+                  <label class="label" for="firstName">first name</label>
+                  <input class="input" name="firstName" type="text" v-model="hero.firstName">
+                </div>
+                <div class="field">
+                  <label class="label" for="lastName">last name</label>
+                  <input class="input" name="lastName" v-model="hero.lastName">
+                </div>
+                <div class="field">
+                  <label class="label" for="description">description</label>
+                  <input class="input" name="description" type="text" v-model="hero.description">
+                </div>
+                <div class="field">
+                  <label class="label" for="capeCounter">cape counter</label>
+                  <input class="input" name="capeCounter" type="number" v-model="hero.capeCounter">
+                </div>
+                <div class="field">
+                  <label class="label" for="capeMessage">cape message</label>
+                  <label class="input" name="capeMessage">{{capeMessage}}</label>
                 </div>
               </div>
-              <footer class="card-footer">
-                <button class="link card-footer-item cancel-button" @click="cancelHero()">
-                  <i class="fas fa-undo"></i>
-                  <span>Cancel</span>
-                </button>
-                <button class="link card-footer-item save-button" @click="saveHero()">
-                  <i class="fas fa-save"></i>
-                  <span>Save</span>
-                </button>
-              </footer>
             </div>
+            <footer class="card-footer">
+              <button class="link card-footer-item cancel-button" @click="cancelHero()">
+                <i class="fas fa-undo"></i>
+                <span>Cancel</span>
+              </button>
+              <button class="link card-footer-item save-button" @click="saveHero()">
+                <i class="fas fa-save"></i>
+                <span>Save</span>
+              </button>
+            </footer>
           </div>
+          <div class="notification is-info" v-show="message">{{message}}</div>
         </div>
       </div>
     </div>
@@ -113,52 +59,67 @@ export default {
   name: "Heroes",
   data() {
     return {
-      selectedHero: undefined,
-      heroes: [
-        {
-          id: 10,
-          name: "Ella",
-          address: "1 Lois Lane",
-          city: "Orlando",
-          state: "FL",
-          description: "fashionista"
-        },
-        {
-          id: 20,
-          name: "Madelyn",
-          address: "1 Somewhere Street",
-          city: "Tampa",
-          state: "FL",
-          description: "the cat whisperer"
-        },
-        {
-          id: 30,
-          name: "Haley",
-          address: "1 Nowhere Trail ",
-          city: "Miami",
-          state: "FL",
-          description: "pen wielder"
-        }
-      ]
+      hero: {},
+      capeMessage: "",
+      message: ""
     };
   },
+  created() {
+    this.loadHero();
+  },
   computed: {
-    fullAddress() {
-      return this.selectedHero
-        ? `${this.selectedHero.address}, ${this.selectedHero.city} ${
-            this.selectedHero.state
-          }`
-        : "";
+    fullName() {
+      return `${this.hero.firstName} ${this.hero.lastName}`;
+    }
+  },
+  watch: {
+    "hero.capeCounter": {
+      immediate: true,
+      handler(newValue, oldValue) {
+        console.log("Watcher evaluated!");
+        const value = parseInt(newValue, 10);
+        switch (value) {
+          case 0:
+            this.capeMessage = "Where is my cape?";
+            break;
+          case 1:
+            this.capeMessage = "One is all I need";
+            break;
+          case 2:
+            this.capeMessage = "Alway have a spare";
+            break;
+          default:
+            this.capeMessage = "You can never have enough capes";
+            break;
+        }
+      }
     }
   },
   methods: {
-    selectHero(hero) {
-      this.selectedHero = hero;
-    },
     cancelHero() {
-      this.selectedHero = undefined;
+      this.loadHero();
     },
-    saveHero() {}
+    async loadHero() {
+      this.hero = {};
+      this.message = "getting the hero, please be patient";
+      this.hero = await this.getHero();
+      this.message = "";
+    },
+    saveHero() {
+      alert("Save!");
+    },
+    async getHero() {
+      const hero = {
+        id: 20,
+        firstName: "Madelyn",
+        lastName: "Papa",
+        capeCounter: 1,
+        description: "the cat whisperer"
+      };
+      return new Promise((resolve, reject) => {
+        setTimeout(() => resolve(hero), 2000);
+      });
+    }
   }
 };
 </script>
