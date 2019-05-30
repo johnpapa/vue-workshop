f64768319a13bf6a437f00ef077d0c8dc5f29d86
John Papa <john@johnpapa.net>
2019-05-30 22:55:49 +0300

    fixed Action calls to new naming convention of Action suffix.
    replaced mapMutations with mapActions

--- labs/09-vuex-state/end/src/views/cart.vue
+++ labs/09-vuex-state/end/src/views/cart.vue
@@ -16,7 +16,7 @@
           <td>{{ item.quantity }}</td>
           <td>{{ '$' + item.total.toFixed(2) }}</td>
           <td>
-            <button @click="removeFromCart(item)">X</button>
+            <button @click="removeFromCartAction(item)">X</button>
           </td>
         </tr>
         <tr class="cartTotalRow">
@@ -32,7 +32,7 @@
 </template>
 
 <script>
-import { mapState, mapGetters, mapMutations } from "vuex";
+import { mapState, mapGetters, mapActions } from "vuex";
 
 export default {
   name: "Cart",
@@ -41,7 +41,7 @@ export default {
     ...mapGetters(["cartTotal"])
   },
   methods: {
-    ...mapMutations(["removeFromCart", "clearCart"])
+    ...mapActions(["removeFromCartAction"])
   }
 };
 </script>
