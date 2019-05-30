dfa8d8ccecf84844f8deb427114d6ed676c435fa
John Papa <john@johnpapa.net>
2019-05-30 21:47:53 +0300

    begin state for http lab

---
+++ labs/07-http/begin/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <h1 class="title">http</h1>
+    <div class="columns">
+      <Heroes class="column"/>
+    </div>
+  </div>
+</template>
+
+<script>
+import Heroes from "./components/heroes.vue";
+
+export default {
+  name: "app",
+  components: {
+    Heroes
+  }
+};
+</script>
+
+<style lang="scss">
+@import "@/design/index.scss";
+</style>
