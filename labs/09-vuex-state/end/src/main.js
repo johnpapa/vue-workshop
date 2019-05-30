d92e2363b366911732221261e23328f852365d86
John Papa <john@johnpapa.net>
2019-05-30 19:02:19 +0300

    prettier

--- labs/09-vuex-state/end/src/main.js
+++ labs/09-vuex-state/end/src/main.js
@@ -1,9 +1,5 @@
 import Vue from 'vue';
-
-// import VueRouter from 'vue-router';
-
 import App from '@/App.vue';
-
 import router from './router'
 import store from './store'
 
--- labs/09-vuex-state/end/src/store.js
+++ labs/09-vuex-state/end/src/store.js
@@ -6,34 +6,34 @@ Vue.use(Vuex);
 const URL = 'http://localhost:8080/products.json';
 
 export default new Vuex.Store({
-	state: {
-		cart: [],
-		products: []
-	},
-	mutations: {
-		addProduct({ products }, product) {
-			products.push(product)
-		},
-		addToCart({ cart }, product) {
-			const itemIndex = cart.findIndex(item => item.id === product.id);
-			if (itemIndex === -1) {
-				cart.push({ ...product, quantity: 1, total: product.price })
-			} else {
+  state: {
+    cart: [],
+    products: []
+  },
+  mutations: {
+    addProduct({ products }, product) {
+      products.push(product);
+    },
+    addToCart({ cart }, product) {
+      const itemIndex = cart.findIndex(item => item.id === product.id);
+      if (itemIndex === -1) {
+        cart.push({ ...product, quantity: 1, total: product.price });
+      } else {
         let cartItem = cart[itemIndex];
         cartItem.quantity++;
         cartItem.total = cartItem.price * cartItem.quantity;
-			}
-		},
-		removeFromCart({ cart }, item) {
-			const itemIndex = cart.findIndex(cartItem => cartItem.id === item.id);
-			cart.splice(itemIndex, 1);
-		},
-		clearCart(state) {
-			state.cart = [];
-		}
-	},
-	actions: {
-		getProducts({ state, commit }) {
+      }
+    },
+    removeFromCart({ cart }, item) {
+      const itemIndex = cart.findIndex(cartItem => cartItem.id === item.id);
+      cart.splice(itemIndex, 1);
+    },
+    clearCart(state) {
+      state.cart = [];
+    }
+  },
+  actions: {
+    getProducts({ state, commit }) {
       if (!state.products.length) {
         fetch(URL)
           .then(res => res.json())
@@ -47,15 +47,15 @@ export default new Vuex.Store({
             }
           });
       }
-		}
-	},
-	getters: {
-		cartTotal: ({ cart }) => {
+    }
+  },
+  getters: {
+    cartTotal: ({ cart }) => {
       let total = 0;
       for (let item of cart) {
         total += item.price * item.quantity;
       }
-			return total;
-		}
-	}
-})
+      return total;
+    }
+  }
+});
